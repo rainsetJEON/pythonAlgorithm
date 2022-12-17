1abb614dc7c236d133deaa00cdbfdecc98b32dde
rainsetJEON <jwj9676@gmail.com>
2022-12-17 15:56:33 +0900

    12/17

--- grid1-1.py
+++ grid1-1.py
@@ -1,64 +1,64 @@
-# a = 'I\'m woojin'
-# b = 'my name is "woojin".'
+a = 'I\'m woojin'
+b = 'my name is "woojin".'
 
-# print(b)
-# print(a[2])
+print(b)
+print(a[2])
 
 # 전체 주석 처리 단축키 : ctrl + /
 
-# L1 = "%d apples" %3
-# print(L1)
-# L2 = ("%d bananas" %4)
-# print(L2)
+L1 = "%d apples" %3
+print(L1)
+L2 = ("%d bananas" %4)
+print(L2)
 
-# num = 12
-# day = "three"
-# L3 = ("%d days, %s babies" %(num, day))
-# print(L3)
+num = 12
+day = "three"
+L3 = ("%d days, %s babies" %(num, day))
+print(L3)
 
 # percent % 표시하기
-# L4 = 'update %-5d%%' %7
-# print(L4)
+L4 = 'update %-5d%%' %7
+print(L4)
 
 # format 함수
-# L5 = "I ate {0} apples, it is {feeling}".format(10, feeling = 'good')
-# print(L5)
+L5 = "I ate {0} apples, it is {feeling}".format(10, feeling = 'good')
+print(L5)
 
 # 공백 채우기
-# L6 = "{0:=^10}".format("hi")
-# print(L6)
+L6 = "{0:=^10}".format("hi")
+print(L6)
 
-# L7 = "{0:=<8}".format("hi")
-# print(L7)
+L7 = "{0:=<8}".format("hi")
+print(L7)
 
 # join
-# ex1 = ".".join('abcd')
-# print(ex1)
+ex1 = ".".join('abcd')
+print(ex1)
 
-# ex2 = ".".join(['a', 'b', 'c'])
-# print(ex2)
+ex2 = ".".join(['a', 'b', 'c'])
+print(ex2)
 
 #p63 연습문제 1,2번
-# p1 = '881120-1068234'
-# birth = p1[0:6]
-# id = p1[7:]
+p1 = '881120-1068234'
+birth = p1[0:6]
+id = p1[7:]
 
-# L8 = "gildong birth : %s, id : %s" %(birth, id)
-# print(L8)
+L8 = "gildong birth : %s, id : %s" %(birth, id)
+print(L8)
 
 # p63 연습문제 3번
-# d = 'a:b:c'
-# d = d.replace(':','#')
-# print(d)
+d = 'a:b:c'
+d = d.replace(':','#')
+print(d)
 
 # p64 연습문제 4번
-# d = 'a:b:c'
-# e = d.split(':')
-# f = "#".join(e)
+d = 'a:b:c'
+e = d.split(':')
+f = "#".join(e)
 
-# print(d)
-# print(e)
-# print(f)
+print(d)
+print(e)
+print(f)
 
 # list
 '''
@@ -71,21 +71,21 @@ list 함수들
 개수 세기 ; count
 확장 ; extend
 '''
-# a = [1, 2, '3', '4']
-# print(a[1])
-# print(a[2] + a[3])
+a = [1, 2, '3', '4']
+print(a[1])
+print(a[2] + a[3])
 
-# a = [1,2,3]
-# a[2] = 4
-# # print(a)
-
-# del a[0]
-# # print(a)
-
-# a.insert(0,5)
+a = [1,2,3]
+a[2] = 4
 # print(a)
 
-# print(a.index(5))
+del a[0]
+# print(a)
+
+a.insert(0,5)
+print(a)
+
+print(a.index(5))
 
 b = [4,5,6]
 b.extend([7,8])
@@ -98,4 +98,16 @@ print(z2)
 print(type(z2))
 
 # tuple 튜플
-print("wonpyo")
+t1 = ('a', 'b', 'c', 1, 2, 3)
+print(t1 * 2)
+
+# p80 연습문제 1번 
+T1 = (1, 2, 3)
+T1 = T1 + (4,)
+print(T1)
+
+
+
+
+
+
