3a989ecdd57a7f9033cd819021ea1a0974f8c194
rainsetJEON <jwj9676@gmail.com>
2023-02-16 19:40:38 +0900

    02/16

--- python_3.py
+++ python_3.py
@@ -34,11 +34,85 @@ a = Calculator() <- a는 객체, a는 클래쓰 Calculator의 인스턴스
 *클래스 안에서 구현된 함수는 Method(메서드)라고 부른다.
 """
 
+# self의 의미 ; 호출 시 호출한 객체 자신이 전달된다.
+
 # 계산기 만들기
 class Fourcal():
     def setdata(self, first, second):   # 메서드의 매개변수
         self.first = first  # 메서드의 수행문
         self.second = second# 메서드의 수행문 
+
+    def add(self):
+        result = self.first + self.second
+        return result
+    
+    def sub(self):
+        result = self.first - self.second
+        return result
+    
+    def mul(self):
+        result = self.first * self.second
+        return result
+    
+    def div(self):
+        result = self.first / self.second
+        return result
+
+"""
+ 생성자(Constructor)
+ ㄴ> 객체가 생성될 때 자동으로 호출되는 메서드
+ ㄴ> 파이썬 메서드명으로 __init__을 사용하면 이 메서드는 생성자가 된다.
+   ㄴ>__init__메서드는 setdata메서드와 이름만 다르고 동일하지만, 
+      생성자로 인식되어 객체가 생성되는 시점에 자동으로 호출
+      따라서 호출 시, 바로 data값과 함께 객체를 생성해야 함
+      ex) a = FourCal(4,2)
+"""
+
+class Fourcal():
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+#   def setdata(self, first, second):   
+#      self.first = first  
+#        self.second = second
+
+    def add(self):
+        result = self.first + self.second
+        return result
+    
+    def sub(self):
+        result = self.first - self.second
+        return result
+    
+    def mul(self):
+        result = self.first * self.second
+        return result
+    
+    def div(self):
+        result = self.first / self.second
+        return result
+
+# 클래스의 상속(Inheritance)
+# class '새로운 클래스'('기존 클래스')
+
+class More_Cal(Fourcal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+# 메서드 오버라이딩(Overriding, 덮어쓰기)
+class SafeFourcal(Fourcal):
+    def div(self):
+        if self.second == 0:
+            return False
+        else:
+            return self.first / self.second
+
+# 클래스 변수
+# 클래스 안에서 선언된 변수로, 클레스에 의해 생성된 모든 객체에 공유된다는 특징을 갖는다.
+
+
 
 
 
